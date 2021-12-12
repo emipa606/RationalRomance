fcdf1b7377c8bec4c57706e5413b67dfa12162bf
Mlie <mlie@litet.net>
2021-12-12 19:58:21 +0100

    Polyamorous pawns with polyamorous fiances should no longer break the game when getting hit on by other pawns

--- Source/Rainbeau's Rational Romance/InteractionWorker_RomanceAttempt_BreakLoverAndFianceRelations.cs
+++ Source/Rainbeau's Rational Romance/InteractionWorker_RomanceAttempt_BreakLoverAndFianceRelations.cs
@@ -3,47 +3,46 @@ using HarmonyLib;
 using RimWorld;
 using Verse;
 
-namespace RationalRomance_Code
+namespace RationalRomance_Code;
+
+[HarmonyPatch(typeof(InteractionWorker_RomanceAttempt), "BreakLoverAndFianceRelations", null)]
+public static class InteractionWorker_RomanceAttempt_BreakLoverAndFianceRelations
 {
-    [HarmonyPatch(typeof(InteractionWorker_RomanceAttempt), "BreakLoverAndFianceRelations", null)]
-    public static class InteractionWorker_RomanceAttempt_BreakLoverAndFianceRelations
+    // CHANGE: Allowed for polyamory.
+    public static bool Prefix(Pawn pawn, ref List<Pawn> oldLoversAndFiances)
     {
-        // CHANGE: Allowed for polyamory.
-        public static bool Prefix(Pawn pawn, ref List<Pawn> oldLoversAndFiances)
+        oldLoversAndFiances = new List<Pawn>();
+        while (true)
         {
-            oldLoversAndFiances = new List<Pawn>();
-            while (true)
+            var firstDirectRelationPawn = pawn.relations.GetFirstDirectRelationPawn(PawnRelationDefOf.Lover);
+            if (firstDirectRelationPawn != null &&
+                (!firstDirectRelationPawn.story.traits.HasTrait(RRRTraitDefOf.Polyamorous) ||
+                 !pawn.story.traits.HasTrait(RRRTraitDefOf.Polyamorous)))
             {
-                var firstDirectRelationPawn = pawn.relations.GetFirstDirectRelationPawn(PawnRelationDefOf.Lover);
-                if (firstDirectRelationPawn != null &&
-                    (!firstDirectRelationPawn.story.traits.HasTrait(RRRTraitDefOf.Polyamorous) ||
-                     !pawn.story.traits.HasTrait(RRRTraitDefOf.Polyamorous)))
-                {
-                    pawn.relations.RemoveDirectRelation(PawnRelationDefOf.Lover, firstDirectRelationPawn);
-                    pawn.relations.AddDirectRelation(PawnRelationDefOf.ExLover, firstDirectRelationPawn);
-                    oldLoversAndFiances.Add(firstDirectRelationPawn);
-                }
-                else
-                {
-                    var firstDirectRelationPawn1 = pawn.relations.GetFirstDirectRelationPawn(PawnRelationDefOf.Fiance);
-                    if (firstDirectRelationPawn1 == null)
-                    {
-                        break;
-                    }
-
-                    if (firstDirectRelationPawn1.story.traits.HasTrait(RRRTraitDefOf.Polyamorous) &&
-                        pawn.story.traits.HasTrait(RRRTraitDefOf.Polyamorous))
-                    {
-                        continue;
-                    }
-
-                    pawn.relations.RemoveDirectRelation(PawnRelationDefOf.Fiance, firstDirectRelationPawn1);
-                    pawn.relations.AddDirectRelation(PawnRelationDefOf.ExLover, firstDirectRelationPawn1);
-                    oldLoversAndFiances.Add(firstDirectRelationPawn1);
-                }
+                pawn.relations.RemoveDirectRelation(PawnRelationDefOf.Lover, firstDirectRelationPawn);
+                pawn.relations.AddDirectRelation(PawnRelationDefOf.ExLover, firstDirectRelationPawn);
+                oldLoversAndFiances.Add(firstDirectRelationPawn);
             }
+            else
+            {
+                var firstDirectRelationPawn1 = pawn.relations.GetFirstDirectRelationPawn(PawnRelationDefOf.Fiance);
+                if (firstDirectRelationPawn1 == null)
+                {
+                    break;
+                }
 
-            return false;
+                if (firstDirectRelationPawn1.story.traits.HasTrait(RRRTraitDefOf.Polyamorous) &&
+                    pawn.story.traits.HasTrait(RRRTraitDefOf.Polyamorous))
+                {
+                    break;
+                }
+
+                pawn.relations.RemoveDirectRelation(PawnRelationDefOf.Fiance, firstDirectRelationPawn1);
+                pawn.relations.AddDirectRelation(PawnRelationDefOf.ExLover, firstDirectRelationPawn1);
+                oldLoversAndFiances.Add(firstDirectRelationPawn1);
+            }
         }
+
+        return false;
     }
 }
